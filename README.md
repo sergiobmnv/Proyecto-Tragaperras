0d59ff9182b98c6e2f00a7c5202402d8819f1a50
Sergio Bravo Mora <146812426+sergiobmnv@users.noreply.github.com>
2024-11-04 13:06:59 +0100

    implementacion del reloj y Informacion del README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Proyecto-Tragaperras
+# Proyecto-Tragaperras - Sergio Bravo Mora.
+
+# Header: 
+# En el header tenemos la parte del logo(Imagen Floky Slots);
+# A su derecha ira un marco para la hora;
+# En el centro estara posicionado el nombre de la tragaperra;
+# En la parte derecha iria los botones de retirar y depositar junto el icono del usuario para cerrar sesion;
+
+# Body:
+# En la seccion del body la he dividido en varias partes:
+# En la parte izquierda pondre un tablon sobre las diferentes simbolos y sus convinaciones;
+# En la parte central estara el tablero de juego;
+# En la parte derecha iria un gif para que la pagina se vea mas llamativa;
+# En la parte supertior del tablero iria todas las opciones de sonido apuestas y boton de girar;
+
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+/* Funcion Modo Oscuro - Claro*/
 const modoIcono = document.getElementById('modoIcono');
         const body = document.body;
 
@@ -14,3 +17,18 @@ const modoIcono = document.getElementById('modoIcono');
                 modoIcono.classList.add('fa-sun'); // Cambia a icono de sol
             }
 });
+
+/* Funcion Reloj */
+
+function actualizarReloj() {
+    const ahora = new Date();
+    const horas = String(ahora.getHours()).padStart(2, '0'); // Formato de 2 dígitos
+    const minutos = String(ahora.getMinutes()).padStart(2, '0');
+    const segundos = String(ahora.getSeconds()).padStart(2, '0');
+    const tiempo = `${horas}:${minutos}:${segundos}`; // Formato HH:MM:SS
+    document.getElementById('horaActual').textContent = tiempo; // Actualizar el contenido del reloj
+}
+
+// Llama a la función al cargar la página y cada segundo
+actualizarReloj(); // Inicializar el reloj
+setInterval(actualizarReloj, 1000); // Actualizar cada segundo
